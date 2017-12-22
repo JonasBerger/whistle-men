6959f294752dd5cd816bc435c5a5146359c86ae1
JonasBerger <Jay-Be1@gmx.de>
2017-12-22 21:57:01 +0100

    updated config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
-/*https://api.github.com/repos/
-JonasBerger/whistle-men/branches/
-master?access_token=b79ff3ed15c4f468f02d845993fde65b3ae8bb11*/
-
-
 export const config = {
 	access_token: "", //create your access-token git->settings->developer-settings-> personal access-tokens
 	baseurl: "https://api.github.com", //self-explaining
